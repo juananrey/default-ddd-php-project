aa66beed9abae023e169b19957608b1ce2ed5ba5
juananrey <ja.reymendo@gmail.com>
2019-02-23 10:14:08 +0100

    SoC The DDD way: part 2

--- config/index.php
+++ config/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Infrastructure\Ui\Web\Symfony\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -31,7 +32,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
-$kernel = new \App\Infrastructure\Ui\Web\Symfony\Kernel($env, $debug);
+$kernel = new Kernel($env, $debug);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
